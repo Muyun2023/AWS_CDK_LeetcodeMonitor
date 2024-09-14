a6c362e3d9d253ec91ce43d32233709903cede2d
VivianDongChen <chen.dong1@northeastern.edu>
2024-09-13 21:36:24 -0700

    Initial commit

---
+++ test/leet_code-progress-email-tracker.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LeetCodeProgressEmailTracker from '../lib/leet_code-progress-email-tracker-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/leet_code-progress-email-tracker-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LeetCodeProgressEmailTracker.LeetCodeProgressEmailTrackerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
